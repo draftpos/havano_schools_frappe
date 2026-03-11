1a8c316e2af254b5e8576902d91468ef7da0d1f5
makonia20 <makonia20@gmail.com>
2026-03-11 17:44:58 +0200

    Fix receipting: add GL entry fix with reload before submit

--- school/school_management/doctype/receipting/receipting.py
+++ school/school_management/doctype/receipting/receipting.py
@@ -26,7 +26,6 @@ class Receipting(Document):
 
         company = frappe.defaults.get_global_default("company")
         student = frappe.get_doc("Student", self.student_name)
-
         company_currency = frappe.db.get_value("Company", company, "default_currency")
         receivable_account = frappe.db.get_value("Company", company, "default_receivable_account")
         paid_to_account = self.account
@@ -60,7 +59,10 @@ class Receipting(Document):
         pe.flags.ignore_permissions = True
         pe.flags.ignore_validate = True
         pe.flags.ignore_mandatory = True
+        pe.flags.ignore_account_permission = True
         pe.insert(ignore_mandatory=True)
+        frappe.db.commit()
+        pe.reload()
         pe.submit()
         self.payment_entry = pe.name
         frappe.msgprint(f"Payment Entry {pe.name} created successfully.")
