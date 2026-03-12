7d8a8e40dc22607a65b329635d09f7cbabdef2a9
makonia20 <makonia20@gmail.com>
2026-03-12 10:42:37 +0200

    Fix receipting JS: remove double submit, fix opening balance load

--- school/school_management/doctype/receipting/receipting.js
+++ school/school_management/doctype/receipting/receipting.js
@@ -12,7 +12,12 @@ frappe.ui.form.on("Receipting", {
                 }
             };
         });
-
+        frm.set_query("student_name", function() {
+            let filters = {};
+            if (frm.doc.student_class) filters.student_class = frm.doc.student_class;
+            if (frm.doc.section) filters.section = frm.doc.section;
+            return { filters: filters };
+        });
     },
     onload(frm) {
         if (frm.doc.__islocal && !frm.doc.date) {
@@ -21,16 +26,15 @@ frappe.ui.form.on("Receipting", {
     },
     student_name(frm) {
         if (frm.doc.student_name) {
-            frappe.call({
-                method: "frappe.client.get",
-                args: { doctype: "Student", name: frm.doc.student_name },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value("student_class", r.message.student_class);
-                        frm.set_value("section", r.message.section);
+            frappe.db.get_value("Student", frm.doc.student_name,
+                ["full_name", "student_class", "section"],
+                function(r) {
+                    if (r) {
+                        if (r.student_class) frm.set_value("student_class", r.student_class);
+                        if (r.section) frm.set_value("section", r.section);
                     }
                 }
-            });
+            );
             load_student_invoices(frm);
         } else {
             frm.clear_table("invoice");
@@ -40,6 +44,22 @@ frappe.ui.form.on("Receipting", {
             frm.set_value("total_balance", 0);
         }
     },
+    student_class(frm) {
+        frm.set_query("student_name", function() {
+            let filters = {};
+            if (frm.doc.student_class) filters.student_class = frm.doc.student_class;
+            if (frm.doc.section) filters.section = frm.doc.section;
+            return { filters: filters };
+        });
+    },
+    section(frm) {
+        frm.set_query("student_name", function() {
+            let filters = {};
+            if (frm.doc.student_class) filters.student_class = frm.doc.student_class;
+            if (frm.doc.section) filters.section = frm.doc.section;
+            return { filters: filters };
+        });
+    }
 });
 
 frappe.ui.form.on("Receipt Item", {
@@ -54,12 +74,14 @@ frappe.ui.form.on("Receipt Item", {
 function load_student_invoices(frm) {
     frm.clear_table("invoice");
     frm.refresh_field("invoice");
-    frappe.call({
-        method: "frappe.client.get",
-        args: { doctype: "Student", name: frm.doc.student_name },
-        callback: function(r) {
-            if (!r.message) return;
-            let student = r.message;
+    frappe.show_alert({message: "Loading invoices...", indicator: "blue"});
+
+    frappe.db.get_value("Student", frm.doc.student_name,
+        ["full_name", "has_opening_balance", "opening_balance"],
+        function(student) {
+            if (!student) return;
+
+            // Add opening balance row if applicable
             if (student.has_opening_balance && student.opening_balance > 0) {
                 let row = frm.add_child("invoice");
                 row.invoice_number = "";
@@ -68,21 +90,14 @@ function load_student_invoices(frm) {
                 row.outstanding = student.opening_balance;
                 row.allocated = 0;
             }
+
+            // Load outstanding Sales Invoices
             frappe.call({
-                method: "frappe.client.get_list",
-                args: {
-                    doctype: "Sales Invoice",
-                    filters: {
-                        customer: student.full_name,
-                        docstatus: 1,
-                        outstanding_amount: [">", 0]
-                    },
-                    fields: ["name", "grand_total", "outstanding_amount", "remarks", "fees_structure"],
-                    order_by: "posting_date asc"
-                },
-                callback: function(r2) {
-                    if (r2.message && r2.message.length > 0) {
-                        r2.message.forEach(function(inv) {
+                method: "school.api.get_student_invoices",
+                args: { student: frm.doc.student_name },
+                callback: function(res) {
+                    if (res.message && res.message.length) {
+                        res.message.forEach(function(inv) {
                             let row = frm.add_child("invoice");
                             row.invoice_number = inv.name;
                             row.fees_structure = inv.fees_structure || inv.remarks || "";
@@ -90,13 +105,16 @@ function load_student_invoices(frm) {
                             row.outstanding = inv.outstanding_amount;
                             row.allocated = 0;
                         });
+                        frappe.show_alert({message: res.message.length + " invoice(s) loaded", indicator: "green"});
+                    } else {
+                        frappe.show_alert({message: "No outstanding invoices found", indicator: "orange"});
                     }
                     frm.refresh_field("invoice");
                     calculate_totals(frm);
                 }
             });
         }
-    });
+    );
 }
 
 function calculate_totals(frm) {
