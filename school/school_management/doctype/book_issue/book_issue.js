346c166a583991c51d535714b90e49b1bf88e9e4
makonia20 <makonia20@gmail.com>
2026-05-28 21:30:08 +0200

    fix: change book_number to Select type

--- school/school_management/doctype/book_issue/book_issue.js
+++ school/school_management/doctype/book_issue/book_issue.js
@@ -1,22 +1,32 @@
 frappe.ui.form.on('Book Issue', {
-	book_title: function(frm) {
+	refresh: function(frm) {
 		if (frm.doc.book_title) {
-			frappe.call({
-				method: 'frappe.client.get',
-				args: {
-					doctype: 'Books',
-					name: frm.doc.book_title
-				},
-				callback: function(r) {
-					if (r.message && r.message.items) {
-						let options = r.message.items.map(i => i.book_number).filter(Boolean);
-						frm.set_df_property('book_number', 'options', options);
-					}
-				}
-			});
-		} else {
-			frm.set_df_property('book_number', 'options', []);
-			frm.set_value('book_number', '');
+			frm.trigger('fetch_book_numbers');
 		}
+	},
+	book_title: function(frm) {
+		frm.set_value('book_number', '');
+		if (frm.doc.book_title) {
+			frm.trigger('fetch_book_numbers');
+		} else {
+			frm.set_df_property('book_number', 'options', '');
+		}
+	},
+	fetch_book_numbers: function(frm) {
+		frappe.call({
+			method: 'frappe.client.get',
+			args: {
+				doctype: 'Books',
+				name: frm.doc.book_title
+			},
+			callback: function(r) {
+				if (r.message && r.message.items) {
+					let options = r.message.items.map(i => i.book_number).filter(Boolean);
+					frm.set_df_property('book_number', 'options', options.join('\n'));
+				} else {
+					frm.set_df_property('book_number', 'options', '');
+				}
+			}
+		});
 	}
 });
