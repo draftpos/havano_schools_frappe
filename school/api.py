94323b9a1f378ab5129ab182a50ceedc9cf44a0a
makonia20 <makonia20@gmail.com>
2026-07-21 16:12:54 +0200

    Fetch student_reg_no instead of ID and display 0 pts for A Level U grade

--- school/api.py
+++ school/api.py
@@ -666,6 +666,7 @@ def get_term_exam_results(student=None):
                 item["student_name"] = student.full_name or s_name
             if "student" not in item or not item["student"]:
                 item["student"] = student.student_reg_no or s_name
+            item["student_reg_no"] = student.student_reg_no
 
         # Get school name from cost center
         school_name = ""
