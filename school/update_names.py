ee1ab764b588efc84aeb2ff50596725906f1a543
makonia20 <makonia20@gmail.com>
2026-08-03 16:04:15 +0200

    Fix update names script

--- school/update_names.py
+++ school/update_names.py
@@ -10,15 +10,6 @@ def update_existing_student_names():
         WHERE (t.student_name IS NULL OR t.student_name = '' OR t.student_name = t.student)
     """)
     
-    # Also update Exam Schedule Item if needed
-    print("Updating Exam Schedule Item...")
-    frappe.db.sql("""
-        UPDATE `tabExam Schedule Item` t
-        JOIN `tabStudent` s ON t.student_admission_no = s.name
-        SET t.student_full_name = s.full_name
-        WHERE (t.student_full_name IS NULL OR t.student_full_name = '' OR t.student_full_name = t.student_admission_no)
-    """)
-    
     frappe.db.commit()
     print("Update complete!")
 
