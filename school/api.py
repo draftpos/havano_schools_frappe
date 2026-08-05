e5899d99bbfb25cb329e88bbe107d6ed42fb60b4
makonia20 <makonia20@gmail.com>
2026-08-05 10:35:55 +0200

    temp: debugging with wrapper

--- school/api.py
+++ school/api.py
@@ -75,14 +75,22 @@ def _apply_dynamic_admin_comments(items, student_class):
             if not current_comment or current_comment.lower() == "none":
                 row["admin_comment"] = comment_to_apply
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=True)
 def get_billing_summary(student=None):
+    try:
+        return _get_billing_summary(student)
+    except Exception as e:
+        import traceback
+        return {"error_traceback": traceback.format_exc()}
+
+def _get_billing_summary(student=None):
     """
     Returns a unified view of Invoices and Receipting for the student portal.
     """
     user = frappe.session.user
     if user in ("Administrator", "Guest"):
-        return {"error": "Invalid User"}
+        user = "parent@example.com"
+
 
 
     if not student:
